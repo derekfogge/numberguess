d508b5f831b86299d975216a6d7294f3dce948b2
derekfogge <dfogge@me.com>
2013-04-29 23:52:55 -0400

    clean up js indenting

--- scripts.js
+++ scripts.js
@@ -1,20 +1,20 @@
 $(document).ready(function() {
 
-  //site picks random number btwn 1 and 10 on page load
-  //and stores it in the variable "randomNum"
-  function generateNum(min, max) {
-  	return Math.floor(Math.random() * (max - min + 1)) + min;
-  }
-  var randomNum = generateNum(1, 10);
+	//site picks random number btwn 1 and 10 on page load
+	//and stores it in the variable "randomNum"
+	function generateNum(min, max) {
+		return Math.floor(Math.random() * (max - min + 1)) + min;
+	}
+	var randomNum = generateNum(1, 10);
 
-  console.log(randomNum);
+	console.log(randomNum);
 
 
-  //this validates the form and checks for a match
-  $('form').submit(function() {
-  	var userNum = $('#guessinput').val();
+	//this validates the form and checks for a match
+	$('form').submit(function() {
+		var userNum = $('#guessinput').val();
 
-  	if (userNum == randomNum) {
+		if (userNum == randomNum) {
 		  console.log('match');
 		} else if (isNaN(userNum)) {
 		 console.log('not a number');
@@ -24,12 +24,12 @@ $(document).ready(function() {
 		  console.log('too high');
 		};
 
-  	console.log(userNum);
+		console.log(userNum);
 
-  	//this clears the input on submit
+		//this clears the input on submit
 		$('#guessinput').val('');
 
-  	return false;
+		return false;
 
 	});
 
